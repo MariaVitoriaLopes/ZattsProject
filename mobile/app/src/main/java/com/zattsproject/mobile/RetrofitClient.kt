871ef5bc2f64d6c6df3c8643659c73497eedf83f
Maria Vitória <lopesmariavitoriamv@gmail.com>
2026-09-19 10:43:50 -0300

    Commit inicial, conexões realizadas

---
+++ mobile/app/src/main/java/com/zattsproject/mobile/RetrofitClient.kt
@@ -0,0 +1,16 @@
+package com.zattsproject.mobile
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    private const val BASE_URL = "http://10.0.2.2:8081/"
+
+    val instance: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
